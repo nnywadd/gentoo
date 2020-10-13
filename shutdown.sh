9a73ffe376e172d05dcdaf0b8c931776619f9bfc
nnywadd <47506905+nnywadd@users.noreply.github.com>
2020-10-13 11:31:04 -0400

    Add files via upload

---
+++ shutdown.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Simple script to handle a DIY shutdown menu. When run you should see a bunch of options (shutdown, reboot etc.)
+#
+# Requirements:
+# - rofi
+# - systemd, but you can replace the commands for OpenRC or anything else
+#
+# Instructions:
+# - Save this file as power.sh or anything
+# - Give it exec priviledge, or chmod +x /path/to/power.sh
+# - Run it
+
+chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot" | rofi -dmenu -i)
+# Info about some states are available here:
+# https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
+
+if [[ $chosen = "Logout" ]]; then
+	bspc quit
+elif [[ $chosen = "Shutdown" ]]; then
+	poweroff
+elif [[ $chosen = "Reboot" ]]; then
+        reboot
+fi
